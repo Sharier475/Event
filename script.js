1e812321f31de8f95acfda966dfb9c060828dd26
SHARIER ISLAM <Sharierpulok475@gmail.com>
2025-05-10 22:13:13 +0600

    Refactor button click event handling to use addEventListener for better flexibility

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-let btn1 = document.querySelector("#btn1");
-btn1.onclick = () =>{
-    console.log("You just Clicked ");
-    let a = 1;
-    a++;
-    console.log(a);
-};
+// let btn1 = document.querySelector("#btn1");
+// btn1.onclick = () =>{
+//     console.log("You just Clicked ");
+//     let a = 1;
+//     a++;
+//     console.log(a);
+// };
 
-let div = document.querySelector("div");
-div.onmouseover = () =>{
-    console.log("You are Inside the Div");
-}
+// let div = document.querySelector("div");
+// div.onmouseover = () =>{
+//     console.log("You are Inside the Div");
+// }
+
+let btn1 = document.querySelector("#btn1");
+ btn1.addEventListener("click",()=>{
+console.log("button1 was Clicked - handler1");
+});
+ btn1.addEventListener("click",()=>{
+console.log("button1 was Clicked - handler2");
+});
+const handler3 = () =>{
+console.log("button1 was Clicked - handler3");
+};
+ btn1.addEventListener("click",handler3);
+
+
+btn1.addEventListener("click",()=>{
+console.log("button1 was Clicked - handler4");
+});
+ btn1.addEventListener("click",()=>{
+console.log("button1 was Clicked - handler3");
+});
+
+btn1.removeEventListener("click",handler3)
